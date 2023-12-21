c401d493dbfe307a96de103f10ec71317adb59a9
Nils Deičmanis <90110509+Nils5678@users.noreply.github.com>
2023-12-21 20:03:56 +0200

    Add files via upload
    Parasta vizuālā programma ar pogām, kas vēl nav uzprogrammētas.

---
+++ AkmensSkeresPapiritsDevBuild1/AkmensSkeresPapirits/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace AkmensSkeresPapirits
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butStart = new System.Windows.Forms.Button();
+            this.labPlayer = new System.Windows.Forms.Label();
+            this.labComputer = new System.Windows.Forms.Label();
+            this.labScore = new System.Windows.Forms.Label();
+            this.butInfo = new System.Windows.Forms.Button();
+            this.picboxComputer = new System.Windows.Forms.PictureBox();
+            this.picboxPlayer = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // butStart
+            // 
+            this.butStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.butStart.Location = new System.Drawing.Point(390, 500);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(100, 50);
+            this.butStart.TabIndex = 0;
+            this.butStart.Text = "Sākt";
+            this.butStart.UseVisualStyleBackColor = true;
+            // 
+            // labPlayer
+            // 
+            this.labPlayer.AutoSize = true;
+            this.labPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labPlayer.Location = new System.Drawing.Point(60, 80);
+            this.labPlayer.Name = "labPlayer";
+            this.labPlayer.Size = new System.Drawing.Size(95, 24);
+            this.labPlayer.TabIndex = 3;
+            this.labPlayer.Text = "Spēlētājs";
+            // 
+            // labComputer
+            // 
+            this.labComputer.AutoSize = true;
+            this.labComputer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labComputer.Location = new System.Drawing.Point(730, 80);
+            this.labComputer.Name = "labComputer";
+            this.labComputer.Size = new System.Drawing.Size(81, 25);
+            this.labComputer.TabIndex = 4;
+            this.labComputer.Text = "Dators";
+            // 
+            // labScore
+            // 
+            this.labScore.AutoSize = true;
+            this.labScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labScore.Location = new System.Drawing.Point(380, 150);
+            this.labScore.Name = "labScore";
+            this.labScore.Size = new System.Drawing.Size(125, 73);
+            this.labScore.TabIndex = 5;
+            this.labScore.Text = "0:0";
+            // 
+            // butInfo
+            // 
+            this.butInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butInfo.Location = new System.Drawing.Point(755, 500);
+            this.butInfo.Name = "butInfo";
+            this.butInfo.Size = new System.Drawing.Size(120, 50);
+            this.butInfo.TabIndex = 6;
+            this.butInfo.Text = "Spēles Noteikumi";
+            this.butInfo.UseVisualStyleBackColor = true;
+            // 
+            // picboxComputer
+            // 
+            this.picboxComputer.Image = global::AkmensSkeresPapirits.Properties.Resources.Custom_Question_mark2;
+            this.picboxComputer.Location = new System.Drawing.Point(675, 110);
+            this.picboxComputer.Name = "picboxComputer";
+            this.picboxComputer.Size = new System.Drawing.Size(200, 150);
+            this.picboxComputer.TabIndex = 2;
+            this.picboxComputer.TabStop = false;
+            // 
+            // picboxPlayer
+            // 
+            this.picboxPlayer.Image = global::AkmensSkeresPapirits.Properties.Resources.Custom_Question_mark2;
+            this.picboxPlayer.Location = new System.Drawing.Point(10, 110);
+            this.picboxPlayer.Name = "picboxPlayer";
+            this.picboxPlayer.Size = new System.Drawing.Size(200, 150);
+            this.picboxPlayer.TabIndex = 1;
+            this.picboxPlayer.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(10, 502);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Autors";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 557);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.butInfo);
+            this.Controls.Add(this.labScore);
+            this.Controls.Add(this.labComputer);
+            this.Controls.Add(this.labPlayer);
+            this.Controls.Add(this.picboxComputer);
+            this.Controls.Add(this.picboxPlayer);
+            this.Controls.Add(this.butStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "Form1";
+            this.Text = "AkmensSķēresPapīrīts";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butStart;
+        private System.Windows.Forms.PictureBox picboxPlayer;
+        private System.Windows.Forms.PictureBox picboxComputer;
+        private System.Windows.Forms.Label labPlayer;
+        private System.Windows.Forms.Label labComputer;
+        private System.Windows.Forms.Label labScore;
+        private System.Windows.Forms.Button butInfo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AkmensSkeresPapiritsDevBuild1/AkmensSkeresPapirits/Form2.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace AkmensSkeresPapirits
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labScore = new System.Windows.Forms.Label();
+            this.labComputer = new System.Windows.Forms.Label();
+            this.labPlayer = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.butRock = new System.Windows.Forms.Button();
+            this.butScissors = new System.Windows.Forms.Button();
+            this.butPaper = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labScore
+            // 
+            this.labScore.AutoSize = true;
+            this.labScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labScore.Location = new System.Drawing.Point(380, 150);
+            this.labScore.Name = "labScore";
+            this.labScore.Size = new System.Drawing.Size(125, 73);
+            this.labScore.TabIndex = 11;
+            this.labScore.Text = "0:0";
+            // 
+            // labComputer
+            // 
+            this.labComputer.AutoSize = true;
+            this.labComputer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labComputer.Location = new System.Drawing.Point(730, 80);
+            this.labComputer.Name = "labComputer";
+            this.labComputer.Size = new System.Drawing.Size(81, 25);
+            this.labComputer.TabIndex = 10;
+            this.labComputer.Text = "Dators";
+            // 
+            // labPlayer
+            // 
+            this.labPlayer.AutoSize = true;
+            this.labPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labPlayer.Location = new System.Drawing.Point(60, 80);
+            this.labPlayer.Name = "labPlayer";
+            this.labPlayer.Size = new System.Drawing.Size(95, 24);
+            this.labPlayer.TabIndex = 9;
+            this.labPlayer.Text = "Spēlētājs";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(675, 110);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 150);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 150);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // butRock
+            // 
+            this.butRock.BackColor = System.Drawing.Color.Red;
+            this.butRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butRock.Location = new System.Drawing.Point(321, 276);
+            this.butRock.Name = "butRock";
+            this.butRock.Size = new System.Drawing.Size(250, 85);
+            this.butRock.TabIndex = 12;
+            this.butRock.Text = "Akmens";
+            this.butRock.UseVisualStyleBackColor = false;
+            // 
+            // butScissors
+            // 
+            this.butScissors.BackColor = System.Drawing.Color.Lime;
+            this.butScissors.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butScissors.Location = new System.Drawing.Point(321, 369);
+            this.butScissors.Name = "butScissors";
+            this.butScissors.Size = new System.Drawing.Size(250, 85);
+            this.butScissors.TabIndex = 13;
+            this.butScissors.Text = "Šķēres";
+            this.butScissors.UseVisualStyleBackColor = false;
+            // 
+            // butPaper
+            // 
+            this.butPaper.BackColor = System.Drawing.Color.Blue;
+            this.butPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butPaper.Location = new System.Drawing.Point(321, 460);
+            this.butPaper.Name = "butPaper";
+            this.butPaper.Size = new System.Drawing.Size(250, 85);
+            this.butPaper.TabIndex = 14;
+            this.butPaper.Text = "Papīrīts";
+            this.butPaper.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(385, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 31);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Piemērs";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 557);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butPaper);
+            this.Controls.Add(this.butScissors);
+            this.Controls.Add(this.butRock);
+            this.Controls.Add(this.labScore);
+            this.Controls.Add(this.labComputer);
+            this.Controls.Add(this.labPlayer);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "Form2";
+            this.Text = "AkmensŠķēresPapirīts";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labScore;
+        private System.Windows.Forms.Label labComputer;
+        private System.Windows.Forms.Label labPlayer;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button butRock;
+        private System.Windows.Forms.Button butScissors;
+        private System.Windows.Forms.Button butPaper;
+        private System.Windows.Forms.Label label1;
+    }
+}
